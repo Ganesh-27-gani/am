87b0f601cdb17f4918205333369e1391e605127d
ganesh sirasani <ganeshsirasani@gmail.com>
2026-07-12 21:56:25 +0530

    first

--- fashion/server.js
+++ fashion/server.js
@@ -2,17 +2,18 @@ import express from "express";
 import dotenv from "dotenv"
 import cors from "cors"
 import contactRoutes  from "./Routes/contactRoutes.js";
+import connectDB from "./config/db.js";
 
 const app = express()
 
 dotenv.config();
+connectDB()
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extends : true }));
 
 app.use("/api/am", contactRoutes)
-
 
 app.get("/",(req,res)=>{
 console.log("hi this ganesh")
