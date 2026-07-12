349911a41e89c773db3be2d4eb53f48d14630f53
ganesh sirasani <ganeshsirasani@gmail.com>
2026-07-12 22:48:19 +0530

    first

--- fashion/server.js
+++ fashion/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extends : true }));
 
-app.use("/api/am", contactRoutes)
+app.use("/api/amfashion", contactRoutes)
 
 app.get("/",(req,res)=>{
 console.log("hi this ganesh")
