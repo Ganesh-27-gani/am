9d2ba70a3e8f9be8fe9ef07686e246253bbbe2d7
ganesh sirasani <ganeshsirasani@gmail.com>
2026-07-17 22:59:31 +0530

    model

--- fashion/models/authmodel.js
+++ fashion/models/authmodel.js
@@ -10,12 +10,37 @@ const authSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         trim: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/^[6-9]\d{9}$/, "Plese enter a valid 10-digit phone number"],
     },
     password: {
         type: String,
         required: true,
         trim: true,
+    },
+    verifyMethod: {
+        type: String,
+        enum: ["email", "phone"],
+        default: "email",
+    },
+    otp: String,
+    otpExpires: Date,
+
+    isVerified: {
+        type: true,
+        default: false,
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
     },
 
 },
