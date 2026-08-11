28b8e254820128e11ed86f1607660ad0b2ce436c
ganesh sirasani <ganeshsirasani@gmail.com>
2026-08-11 17:35:09 +0530

    sendotp

--- fashion/Utils/sendPhone.js
+++ fashion/Utils/sendPhone.js
@@ -6,7 +6,7 @@ const sendPhoneOTP = async (phone, otp) => {
     console.log("SID:", process.env.TWILIO_ACCOUNT_SID);
     console.log("TOKEN:", process.env.TWILIO_AUTH_TOKEN);
     console.log("PHONE:", process.env.TWILIO_PHONE_NUMBER);
-
+ 
     const client = twilio(
         process.env.TWILIO_ACCOUNT_SID,
         process.env.TWILIO_AUTH_TOKEN,
