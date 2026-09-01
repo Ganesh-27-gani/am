04057e17f2f3f5a7636e9477948f7133866f675a
ganesh sirasani <ganeshsirasani@gmail.com>
2026-09-01 19:58:30 +0530

    home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import image from "../assets/m4.png"
+import React from "react";
+import image from "../assets/m4.png";
+import { FaArrowRight } from "react-icons/fa6";
+import "../styles/Home.css"
 
 const Home = () => {
     return (
-        <>
-            <div className="controlar">
+        <section className="home"style={{ backgroundImage: `url(${image})` }}>
+            <div className="home-overlay"></div>
+            <div className="home-container">
+                <div className="home-content">
 
-                <img src={image} alt="" />
+                    <h1>
+                        <span>AM</span> ENTERPRISES
+                    </h1>
+
+                    <h5>
+                        Trading. Import & Export. Supply Chain.
+                        <br />
+                        Business Solutions.
+                    </h5>
+
+                    <p>
+                        Delivering quality products and reliable services
+                        <br />
+                        across the globe.
+                    </p>
+
+                    <button>
+                        GET IN TOUCH
+                        <FaArrowRight />
+                    </button>
+                </div>
             </div>
+        </section>
+    );
+};
 
-        </>
-    )
-}
-
-export default Home
+export default Home;
